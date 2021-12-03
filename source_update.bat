82eae0136c7c49bc8f3969760cea3d81101b0453
Shkiper228 <57232917+Shkiper228@users.noreply.github.com>
2021-12-03 19:53:25 +0200

    update

--- source_update.bat
+++ source_update.bat
@@ -1,8 +1,8 @@
 cd C:\Users\vovam\Desktop\Weisttil\Morgan JS 2.0
 git add .
 git commit -m "update"
-git branch -M main
-git push -u origin main
+git branch -M master
+git push -u origin master
 
 TIMEOUT /T 9
 
