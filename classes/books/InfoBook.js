96f6826b8897d617baaba8dba28d9a6aef6fb2c8
unknown <vovamyrosh2003@gmail.com>
2022-02-18 23:31:35 +0200

    patch

--- classes/books/InfoBook.js
+++ classes/books/InfoBook.js
@@ -78,7 +78,7 @@ class InfoBook extends Book {
                 }]});
             } else {
                 const embed = new MessageEmbed({
-                    title: this.files[pageNumber].slice(-3),
+                    title: this.files[pageNumber].slice(0, -3),
 
                 })
                 this.pages[pageNumber].forEach((page, index) => {
